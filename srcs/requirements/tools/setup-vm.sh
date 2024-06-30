b4f5e488c9624f263ea8eaf1964492fd90eed5c8
luuk <lvan-gef@student.codam.nl>
2024-07-01 00:44:46 +0200

    Fixed setup script

--- srcs/requirements/tools/setup-vm.sh
+++ srcs/requirements/tools/setup-vm.sh
@@ -6,20 +6,19 @@
 # have to install git manually because the minimal version of debian dont ship with it by default
 # and clone it manually
 
-# update the system
-apt update && apt upgrade -y && apt install sudo vim -y
+# update and install deps
+apt update && apt upgrade -y && apt install sudo vim git ca-certificates curl -y
 
 # add user luuk so we dont have to login as root all the time
 # todo: see if we can restrict luuk from what it can do with sudo??
-# usermod -aG sudo luuk
-# adder="luuk ALL=(ALL:ALL) NOPASSWD: ALL"
-# grep "$adder" /etc/sudoers
-# if [ $? -ne 0 ]; then
-# 	echo "$adder" >> /etc/sudoers
-# fi
+usermod -aG sudo luuk
+adder="luuk ALL=(ALL:ALL) NOPASSWD: ALL"
+grep "$adder" /etc/sudoers
+if [ $? -ne 0 ]; then
+	echo "$adder" >> /etc/sudoers
+fi
 
 # https://docs.docker.com/engine/install/debian/
-apt install ca-certificates curl -y
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -37,9 +36,3 @@ apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-co
 # add user luuk to docker
 groupadd docker
 usermod -aG docker luuk
-
-# auto login for new user
-# str='ExecStart=-/sbin/agetty --autologin luuk --noclear %I $TERM'
-# file='/etc/systemd/system/getty.target.wants/getty@tty1.service'
-# sed -i "/^ExecStart=/c\\$str" "$file"
-
