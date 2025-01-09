0c329c8e41a2febd33ebc30557496c423d434e2b
Luuk Van Geffen <lvan-gef@f0r6s2.codam.nl>
2025-01-09 20:57:25 +0100

    changed it

--- srcs/requirements/nginx/tools/ssl_setup.sh
+++ srcs/requirements/nginx/tools/ssl_setup.sh
@@ -11,4 +11,4 @@ mkdir -p /etc/nginx/ssl
 openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
     -keyout $SSL_KEY \
     -out $SSL_CERT \
-    -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=42/CN=localhost"
+    -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=42/CN=lvan-gef.42.fr"
