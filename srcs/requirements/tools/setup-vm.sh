9c07ac24412a46c6d5eb5d5cdcb85b297ca8dc4e
Luuk Van Geffen <lvan-gef@f1r3s12.codam.nl>
2025-01-14 20:39:00 +0100

    Update setup_script

--- srcs/requirements/tools/setup-vm.sh
+++ srcs/requirements/tools/setup-vm.sh
@@ -1,22 +1,10 @@
 #! /bin/bash
 
+# for debian
 # this script will be run as root after the first boot of the vm
-# have to do it manually
-
-# have to install git manually because the minimal version of debian dont ship with it by default
-# and clone it manually
 
 # update and install deps
-apt update && apt upgrade -y && apt install sudo vim git ca-certificates curl rsync -y
-
-# add user luuk so we dont have to login as root all the time
-# todo: see if we can restrict luuk from what it can do with sudo??
-# usermod -aG sudo luuk
-# adder="luuk ALL=(ALL:ALL) NOPASSWD: ALL"
-# grep "$adder" /etc/sudoers
-# if [ $? -ne 0 ]; then
-# 	echo "$adder" >> /etc/sudoers
-# fi
+apt update && apt upgrade -y && apt install sudo git ca-certificates curl rsync make -y
 
 # https://docs.docker.com/engine/install/debian/
 install -m 0755 -d /etc/apt/keyrings
@@ -30,13 +18,24 @@ echo \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 
-# install docker
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# install docker docker-compose and all deps
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose docker-ce-rootless-extras dbus-user-session slirp4netns docker-ce-rootless-extras uidmap -y
 
-# docker-compose
-curl -SL https://github.com/docker/compose/releases/download/v2.32.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+# run docker root-less script  switch to normal user
+sh /usr/bin/dockerd-rootless-setuptool.sh install
+# export PATH=/usr/bin:$PATH
+# export DOCKER_HOST=unix:///run/user/1000/docker.sock
 
-# add user luuk to docker
-groupadd docker
-usermod -aG docker luuk
+# setup neovim
+apt install ninja-build gettext cmake curl build-essential gcc -y
+mkdir /home/inception/build_dir
+cd /home/inception
+git clone https://github.com/neovim/neovim.git
+cd neovim
+git checkout stable
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+rsync -az --rsh='ssh -p2222' ~/.config/nvim inception@localhost:/home/inception/.config/
+
+# restart vm
+reboot
